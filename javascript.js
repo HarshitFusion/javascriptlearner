76a1c98cdbb7f81902049687972ba3b9739a70cb
harshitfusion <mauryaharshit016@gmail.com>
2025-01-15 00:03:26 +0530

    simple if else statement using program

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("hello world this is my first js code")
+let num = prompt("input grades of a student");
+if (num >= 80){
+    console.log("grade is A",num);
+}
+else(num >= 70)
+    console.log("grade is B",num);
+
+if (num >= 60) {
+    console.log("grade is C",num);
+} else(num >= 50)
+    console.log("grade is D",num);
+    
+if(num <= 49){
+    console.log("grade is F",num);
+}
